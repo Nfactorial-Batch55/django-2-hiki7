1927b150c7cfca260c78aa771c1f08f9c436db21
hiki7 <rsaliyev03@gmail.com>
2024-09-16 21:02:33 +0500

    add: urls

--- news_service/news/urls.py
+++ news_service/news/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('')
+    path('', views.NewsListView.as_view(), name='news-list'),
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail'),
+    path('add/', views.NewsCreateView.as_view(), name='news-add'),
 ]

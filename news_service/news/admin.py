38bfbe3094c262974a48bbcd282d58594dc2836b
hiki7 <rsaliyev03@gmail.com>
2024-09-16 21:17:49 +0500

    add: admin panel

--- news_service/news/admin.py
+++ news_service/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import News, Comment
 
-# Register your models here.
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('news', 'content', 'created_at')
--- news_service/news_service/urls.py
+++ news_service/news_service/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+from django.contrib import admin
+
 urlpatterns = [
     path('news/', include('news.urls')),
+    path('admin/', admin.site.urls),
 ]

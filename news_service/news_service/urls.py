6df591b26551078f236eea8746a628fb3fccf9b3
hiki7 <rsaliyev03@gmail.com>
2024-09-18 23:28:24 +0500

    add: django debug toolbar configuration

--- news_service/news_service/urls.py
+++ news_service/news_service/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 
 from django.contrib import admin
+from debug_toolbar.toolbar import debug_toolbar_urls
 
 urlpatterns = [
     path('news/', include('news.urls')),
     path('admin/', admin.site.urls),
-]
+] + debug_toolbar_urls()

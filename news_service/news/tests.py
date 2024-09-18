23e60718bf9472ad4c471a979cba1c14da20cfab
hiki7 <rsaliyev03@gmail.com>
2024-09-18 23:05:54 +0500

    add: tests for the news model

--- news_service/news/tests.py
+++ news_service/news/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import News, Comment
 
-# Create your tests here.
+
+class NewsModelTests(TestCase):
+
+    def setUp(self):
+        self.news_with_comments = News.objects.create(title='news with comments', content='content1')
+        Comment.objects.create(news=self.news_with_comments, content='comment1')
+
+        self.news_without_comments = News.objects.create(title='news without comments', content='content2')
+
+    def test_has_comments_true(self):
+        self.assertTrue(self.news_with_comments.has_comments())
+
+    def test_has_comments_false(self):
+        self.assertFalse(self.news_without_comments.has_comments())

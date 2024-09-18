049790acc6af90120b045b920f4e2a809e7a28dd
hiki7 <rsaliyev03@gmail.com>
2024-09-18 22:59:46 +0500

    add: tabular inline and new columns to the news

--- news_service/news/admin.py
+++ news_service/news/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import News, Comment
 
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 5
+
 @admin.register(News)
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created_at')
+    list_display = ('title', 'content', 'created_at', 'has_comments')
+    inlines = [CommentInline]
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
